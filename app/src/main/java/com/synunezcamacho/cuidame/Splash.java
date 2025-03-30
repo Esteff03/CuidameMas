5ef4482744af7f1e58bbe3bb7c7a4a5a914c47dd
Sindy <sindy.nunez@educa.madrid.org>
2025-03-30 20:04:16 +0200

    Splash con logo y animación.

---
+++ app/src/main/java/com/synunezcamacho/cuidame/Splash.java
@@ -0,0 +1,37 @@
+package com.synunezcamacho.cuidame;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Splash extends AppCompatActivity {
+
+    ImageView imagen;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        imagen = findViewById(R.id.logo);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Animation rotate = AnimationUtils.loadAnimation(
+                this, R.anim.slide_down);
+        imagen.startAnimation(rotate);
+    }
+
+
+}
